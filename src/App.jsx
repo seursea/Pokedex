90a8d7cbd8573546980b33456fa49e30a51c174f
Pat <jpatrice.pasacsac@gmail.com>
2026-07-25 22:32:10 +0800

    update Route

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen bg-[#242424] p-4 md:p-8">
       <h1 className="text-6xl font-bold text-center text-[#08ac94] my-4">Pokédex</h1>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Pokedex" element={<Home />} />
           <Route path="/pokemon/:id" element={<PokemonDetail />} />
         </Routes>
     </div>
